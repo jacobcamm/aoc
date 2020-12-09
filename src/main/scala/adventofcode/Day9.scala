7ffee6bf688421051da30d1bd6ce1b25b4566d74
Jacob Camm <jacob.camm@auden.co.uk>
2020-12-09 12:27:02 +0000

    speed up fast

--- src/main/scala/adventofcode/Day9.scala
+++ src/main/scala/adventofcode/Day9.scala
@@ -47,16 +47,27 @@ object Day9 extends App {
                       target: BigDecimal): BigDecimal = {
     val currentValues = values.take(currentPos)
     val currentSum = currentValues.sum
-    if (currentSum == target) currentValues.max + currentValues.min
-    else if (currentPos == values.size - 1)
-      exploitWeakness(values.drop(1), 0, target)
-    else exploitWeakness(values, currentPos + 1, target)
+
+    currentSum match {
+      case currentSum if currentSum == target =>
+        currentValues.max + currentValues.min
+      case currentSum if currentSum > target =>
+        exploitWeakness(values.drop(1), 0, target)
+      case _ => exploitWeakness(values, currentPos + 1, target)
+    }
   }
 
   val weakNumber: BigDecimal =
-    findWeakness(input.take(25), input(25), input.drop(26))
-  val exploitWeakness: BigDecimal = exploitWeakness(input, 0, weakNumber)
+    time(findWeakness(input.take(25), input(25), input.drop(26)))
+  val exploitWeakness: BigDecimal = time(exploitWeakness(input, 0, weakNumber))
 
+  def time[R](block: => R): R = {
+    val t0 = System.currentTimeMillis()
+    val result = block // call-by-name
+    val t1 = System.currentTimeMillis()
+    println("Elapsed time: " + (t1 - t0) + "ms")
+    result
+  }
   println(s"Day 7 Part 1: $weakNumber")
 
   println(s"Day 2 Part 2: $exploitWeakness")
